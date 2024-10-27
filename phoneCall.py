bff6c81bd63eb3f3d43c78f3844a0210cd1b0112
Turan <turanaymis@gmail.com>
2024-10-27 20:44:56 +0300

    Initial commit

---
+++ phoneCall.py
@@ -0,0 +1,34 @@
+import time
+
+from appium import *
+from appium import webdriver
+from typing import Any, Dict
+from appium.options.common import AppiumOptions
+from appium.webdriver.common.appiumby import AppiumBy
+
+cap: Dict[str, Any] = {
+    'platformName': 'Android',
+    'automationName': 'UIAutomator2',
+    'deviceName': 'Android',
+}
+
+url = 'http://localhost:4724'
+
+driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
+
+el = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@content-desc="Phone"]')
+el.click()
+
+time.sleep(2)
+
+driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="key pad").click()
+
+driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text='1']").click()
+driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text='2']").click()
+driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text='3']").click()
+
+driver.find_element(by=AppiumBy.XPATH, value="//android.widget.ImageButton[@content-desc='dial']").click()
+time.sleep(2)
+driver.find_element(by=AppiumBy.XPATH, value="//android.widget.ImageButton[@content-desc='End call']").click()
+time.sleep(2)
+driver.quit()
